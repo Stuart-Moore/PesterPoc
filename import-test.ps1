9f6bf9fa296d775a760bc7673af6c808f748a27c
Stuart Moore <stuart@stuart-moore.com>
2016-12-06 17:58:12 +0000

    Random pester proof of concepts.

---
+++ import-test.ps1
@@ -0,0 +1,8 @@
+﻿Describe "ImportedTest" {
+$testname = 'does something less useful'
+if (($SkipTests.Functions.$functionname.$testname.skip) -ne 'true'){
+    It "$testname" {
+        $true | Should Be $false
+    }
+}
+}
---
+++ test.Tests.ps1
@@ -0,0 +1,16 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+$functionname = ($sut.Split("."))[0]
+$SkipTests = get-content .\shame.json | convertFrom-Json
+
+Describe "test" {
+$testname = 'does something useful'
+if (($SkipTests.Functions.$functionname.$testname.skip) -ne 'true'){
+    It "$testname" {
+        $true | Should Be $false
+    }
+}
+}
+
+. .\import-test.ps1
